cd3cca0a80e3156c97aa92cde5259db7d03658e8
AryanSamadzadaOPHS <72076191+AryanSamadzadaOPHS@users.noreply.github.com>
2021-02-02 11:29:08 +0000

    Delete mad lib.py

--- mad lib.py
+++
@@ -1,7 +0,0 @@
-name1 = input("Enter a name >>>")
-age = input("Enter an age >>>")
-nationality1 = input("Enter a country >>>")
-print("")
-print("")
-print("Once upon a time" ,name1, " ate 400 burgers and died at the age of" ,age, ". A memorial was built in his honour in the white house of" ,nationality1)
-
